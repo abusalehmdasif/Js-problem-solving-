0b55996113dbd5be1383c9fb0c16b099007e3f61
abusalehmdasif <abusaleh11.asif@gmail.com>
2024-05-24 12:37:46 +0600

    edited

---
+++ part2task5.js
@@ -0,0 +1,30 @@
+// let result= 0
+
+// let num1= 24
+// let num2= 26
+
+// num1>num2
+// ?console.log(num1*2)
+// :console.log(num1+num2)
+
+
+// let result= 0
+
+// let num1= 24
+// let num2= 20
+
+// num1>num2
+// ?console.log(num1*2)
+// :console.log(num1+num2)
+
+
+const num1 = Number(prompt('num1'))
+const num2 = Number(prompt('num2'))
+let result= 0
+
+if(num1>num2){
+    console.log(num1*2)
+}
+else{
+    console.log(num1+num2)
+}
