35fef333547e5f7be2be0bbeb2a7ce5b282fa422
abusalehmdasif <abusaleh11.asif@gmail.com>
2024-05-25 21:22:48 +0600

    arrays

---
+++ array.js
@@ -0,0 +1,23 @@
+// const cars= ['audi','BMW','Toyota','nissan'];
+
+// console.log(cars);
+
+// console.log(cars.length);
+
+// console.log(cars[3])
+
+// cars.push('prado');
+
+//  cars.pop();
+
+// console.log(cars.indexOf('audi'))
+
+// foreach loop
+
+const nums=[1,3,5,6,9]
+nums.foreach=(function(num)
+{
+    if(num %2 == 0){
+        console.log(num )
+    }
+})
